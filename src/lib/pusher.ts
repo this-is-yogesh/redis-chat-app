1c33056e111daa3258be0edf8c290addc0cb78a9
Yogesh Singh <ayogesh1214@gmail.com>
2025-03-25 17:58:57 +0530

    pusher cluster change

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -21,14 +21,14 @@ declare global {
 }
 
 export const pusherServer =
-	global.pusherServer ||
-	new PusherServer({
-		appId: process.env.PUSHER_APP_ID!,
-		key: process.env.PUSHER_APP_KEY!,
-		secret: process.env.PUSHER_APP_SECRET!,
-		cluster: "eu",
-		useTLS: true,
-	});
+  global.pusherServer ||
+  new PusherServer({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.PUSHER_APP_KEY!,
+    secret: process.env.PUSHER_APP_SECRET!,
+    cluster: "ap2",
+    useTLS: true,
+  });
 
 export const pusherClient =
-	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "eu" });
+	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "ap2" });
