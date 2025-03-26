470aaacd402a2cd412a0e877621541a812d9ea6a
Yogesh Singh <ayogesh1214@gmail.com>
2025-03-26 16:28:27 +0530

    redis intance

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,14 @@
 import { Redis } from "@upstash/redis";
 
+/*
+This code sets up a connection to a Redis database hosted on Upstash using credentials stored in environment variables. The redis client instance or new object can then be used throughout the application to interact with the Redis database.
+*/
+
+//@upstash/redis - This package provides a client to interact with Redis databases
 export const redis = new Redis({
-	url: process.env.UPSTASH_REDIS_REST_URL,
-	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  //This is the endpoint URL of the Redis database
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  //This is the authentication token needed to access the Redis database
 });
+//new Redis({}) -  This creates a new instance of the Redis client.
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/* so this is used to basically do conditional css , like if this happens then condition1 else condition2*/
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
