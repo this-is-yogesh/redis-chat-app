afdac9776fa750ec1ac18df5bd5028c35acbdadc
Yogesh Singh <ayogesh1214@gmail.com>
2025-03-26 16:20:59 +0530

    auth actions for userid

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -4,28 +4,28 @@ import { redis } from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function checkAuthStatus() {
-	const { getUser } = getKindeServerSession();
-	const user = await getUser();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-	if (!user) return { success: false };
+  if (!user) return { success: false };
 
-	// namespaces are really important to understand in redis
-	const userId = `user:${user.id}`;
+  // namespaces are really important to understand in redis
+  const userId = `user:${user.id}`;
 
-	const existingUser = await redis.hgetall(userId);
+  const existingUser = await redis.hgetall(userId);
 
-	// sign up case: bc user is visiting our platform for the first time
-	if (!existingUser || Object.keys(existingUser).length === 0) {
-		const imgIsNull = user.picture?.includes("gravatar");
-		const image = imgIsNull ? "" : user.picture;
+  // sign up case: bc user is visiting our platform for the first time
+  if (!existingUser || Object.keys(existingUser).length === 0) {
+    const imgIsNull = user.picture?.includes("gravatar");
+    const image = imgIsNull ? "" : user.picture;
 
-		await redis.hset(userId, {
-			id: user.id,
-			email: user.email,
-			name: `${user.given_name} ${user.family_name}`,
-			image: image,
-		});
-	}
+    await redis.hset(userId, {
+      id: user.id,
+      email: user.email,
+      name: `${user.given_name} ${user.family_name}`,
+      image: image,
+    });
+  }
 
-	return { success: true };
+  return { success: true };
 }
